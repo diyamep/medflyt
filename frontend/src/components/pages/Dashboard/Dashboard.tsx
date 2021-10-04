f09e17d015f0bb7b748cf4cd2777e789b5f43a9c
Yuval <yuval.wilchfort@gmail.com>
2021-10-04 21:25:32 +0300

    After bugs fixed

--- frontend/src/components/pages/Dashboard/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard/Dashboard.tsx
@@ -53,14 +53,14 @@ function useDashboard(params: { year: number }) {
         startLoading();
 
         return axios
-            .get<unknown>(endpoint(`reports/${params.year}`))
-            .then((response) => {
-                if (!resType.is(response)) {
+            .get<Report>(endpoint(`reports/${params.year}`))
+            .then((response) => {                
+                if (!resType.is(response.data)) {
                     console.error(PathReporter.report(resType.decode(response)).join(", "));
                     throw new Error("Error");
                 }
 
-                setState({ type: "Resolved", report: response, isRefreshing: false });
+                setState({ type: "Resolved", report: response.data, isRefreshing: false });
             })
             .catch(() => {
                 setState({ type: "Rejected", error: "Error" });
@@ -83,7 +83,7 @@ const Dashboard = () => {
         case "Rejected":
             return <ErrorView message={state.error} onClickRetry={actions.fetchReport} />;
         case "Resolved":
-            return <TableView {...state} />;
+            return <TableView {...state} onClickRefresh={actions.fetchReport}/>;
         default:
             assertNever(state);
             return <></>;
--- server/src/controllers/reportController.ts
+++ server/src/controllers/reportController.ts
@@ -22,21 +22,52 @@ export const getReport = async (req: Request, res: Response) => {
         FROM caregiver
         JOIN visit ON visit.caregiver = caregiver.id
         JOIN patient ON patient.id = visit.patient
+        WHERE date_part('year', visit.date) = $1
     `;
     
+    /* It is possible to concatenate the patient names in the sql query for instance:
+    SELECT caregiver_name, string_agg(patient_name, ', ') AS patients
+    from
+    (
+    SELECT
+                caregiver.id      AS caregiver_id,
+                caregiver.name    AS caregiver_name,
+                patient.id        AS patient_id,
+                patient.name      AS patient_name,
+                visit.date        AS visit_date
+            FROM caregiver
+            JOIN visit ON visit.caregiver = caregiver.id
+            JOIN patient ON patient.id = visit.patient) report
+    GROUP BY 1;
+
+    But I assumed you meant to solve it in the server logics
+    */
+
     let result : QueryResult;
     try {
-        result = await dbUtil.sqlToDB(sql, []);
+        result = await dbUtil.sqlToDB(sql, [req.params.year]);
         const report: Report = {
             year: parseInt(req.params.year),
             caregivers: []
-        };
+        };        
 
-        for ( let row of result.rows) {
+        for ( let row of result.rows) {     
+            
+            // Check if the current caregiver exists
+            const caregiver = report.caregivers.find(c => c.name===row.caregiver_name);
+
+            // If the current record is of a new caregiver
+            if (!caregiver){
             report.caregivers.push({
                 name: row.caregiver_name,
                 patients: [row.patient_name]
-            })
+            })}
+            else {  
+                // Check if the current record is of a new patient
+                if (!caregiver.patients.includes(row.patient_name)){
+                    caregiver.patients.push(row.patient_name);
+                }
+            }
         }
         res.status(200).json(report);
     } catch (error) {
